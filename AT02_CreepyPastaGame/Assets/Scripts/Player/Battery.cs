2c771c9f05290f330c3c9800378a1d255b65d831
CAMERONM44 <cameronmoltoni@gmail.com>
2024-11-21 16:05:49 +0800

    commented code

--- AT02_CreepyPastaGame/Assets/Scripts/Player/Battery.cs
+++ AT02_CreepyPastaGame/Assets/Scripts/Player/Battery.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class Battery : MonoBehaviour
 {
-    public Flashlight torch;
+    public Flashlight torch;        //references the Flashlight class to access the power float of objects using that script
 
+    ///when the collider of the object this component is attached to touches another collider, check if that objects collider has the "Power" tag,
+    //and if so, set the power of the flashlight to 1, and destroy the game object of the other collider.
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Power"))
--- AT02_CreepyPastaGame/Assets/Scripts/Player/Flashlight.cs
+++ AT02_CreepyPastaGame/Assets/Scripts/Player/Flashlight.cs
@@ -4,19 +4,20 @@ using UnityEngine;
 
 public class Flashlight : MonoBehaviour
 {
-    public GameObject torch;
-    public Light light;
-    public bool isOn;
-    public float power = 1;
+    public Light light;         //the light component on the same game object this script is attached to
+    public bool isOn;           //boolean referenced for enabling/disabling the light component
+    public float power = 1;     //a float correlating to the instensity of the light, 1 = max intensity, 0 = no intensity
 
     // Update is called once per frame
     void Update()
     {
+        //if the isOn boolean is true, and the power float is above 0, subtract 0.01 from the intensity float over real time using Time.deltaTime
         if (isOn && power > 0)
         {
             power -= 0.01f * Time.deltaTime;
             light.intensity = Mathf.Clamp01(power);
         }
+        //when the F key is pressed, invert the isOn boolean and set whether the light is enabled to be equal to the isOn bool
         if (Input.GetKeyDown(KeyCode.F))
         {
             isOn = !isOn;
